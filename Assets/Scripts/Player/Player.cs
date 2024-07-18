fee41fe87044374baa22e710a20db36a161eeed1
GG.mp4 <79061640+viliusradz@users.noreply.github.com>
2024-07-18 16:12:28 +0300

    Added a state machine, blend tree, animations (#4)
    * Add: Basic Movement
    Edit: Character rendered in front of background
    
    * Add: Movement animation framework
    
    * add: Player state machine framework, jump calculations, player animations
    
    * add: Dash mechanic
    fix: dash to movement transition

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,94 @@
+using TMPro.EditorUtilities;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class Player : MonoBehaviour
+{
+    [HideInInspector]
+    public PlayerInput input;
+
+
+    public PlayerStateMachine StateMachine { get; set; }
+
+    public PlayerJumpState JumpState { get; set; }
+    public PlayerMoveState MoveState { get; set; }
+    public PlayerDashState DashState { get; set; }
+
+    [Header("Movement")]
+    public float moveSpeed = 1.0f;
+    public float jumpForce = 20.0f;
+    public float dashSpeed = 5.0f;
+    public float dashDuration = 1.0f;
+
+
+    [Header("Animation")]
+    public Animator animator;
+
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+
+        StateMachine = new PlayerStateMachine();
+
+        JumpState = new PlayerJumpState(this, StateMachine);
+        MoveState = new PlayerMoveState(this, StateMachine);
+        DashState = new PlayerDashState(this, StateMachine);
+
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StateMachine.Initialize(MoveState);
+        //input.actions["Move"].performed += Movement;
+        //input.actions["Fire"].performed += Attack;
+    }
+
+    // Player actions are bound in Inspector (Player Input -> Behavior -> Invoke Unity Events) 
+
+    // Todo:
+    // Movement Animation Bind to direction (pass the movement direction to the animator)
+    /*
+    public void Movement(InputAction.CallbackContext obj)
+    {
+        var direction = obj.ReadValue<Vector2>().normalized;
+        print($"I Moved {direction}");
+    }
+    */
+
+    public void Attack(InputAction.CallbackContext obj)
+    {
+        print($"I attacked {obj.action}");
+    }
+
+    public void OnMove(InputValue dir)
+    {
+        print($"I Moved {dir.Get<Vector2>()}");
+    }
+
+    public void OnJump(InputAction.CallbackContext obj)
+    {
+        if (StateMachine.CurrentState != JumpState)
+            StateMachine.ChangeState(JumpState);
+    }
+    
+    public void OnDash(InputAction.CallbackContext obj)
+    {
+        if(StateMachine.CurrentState != DashState)
+            StateMachine.ChangeState(DashState);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // As PlayerInput events are not called every frame we check the value our selves
+        StateMachine.CurrentState.FrameUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        StateMachine.CurrentState.PhysicsUpdate();
+    }
+}
---
+++ Assets/Scripts/Player/States/PlayerJumpState.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJumpState : PlayerState
+{
+    private InputAction moveAction;
+    private const float gConstant = -9.81f;
+
+    private float jumpStartTime;
+    private Vector3 jumpStartPosition;
+
+    public PlayerJumpState(Player player, PlayerStateMachine playerStateMachine) : base(player, playerStateMachine)
+    {
+        moveAction = player.input.actions["Move"];
+    }
+
+    public override void EnterState()
+    {
+        jumpStartTime = Time.time;
+        jumpStartPosition = player.transform.position;
+
+        player.animator.SetFloat("directionX", 0);
+        player.animator.SetFloat("directionY", 10);
+    }
+
+    public override void ExitState()
+    {
+        player.animator.SetFloat("directionX", 0);
+        player.animator.SetFloat("directionY", 0);
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void FrameUpdate()
+    {
+        // Jump Math
+        var t = Time.time - jumpStartTime;
+        float y = player.jumpForce * t + (gConstant * Mathf.Pow(t, 2)) / 2.0f;
+        if (y < 0)
+        {
+            player.transform.position = jumpStartPosition;
+            player.StateMachine.ChangeState(player.MoveState);
+            return;
+        }
+
+        // Check if player is trying to move left or right
+        var direction = moveAction.ReadValue<Vector2>().x;
+
+        jumpStartPosition.x += direction * player.moveSpeed * Time.deltaTime;
+
+        player.transform.position = jumpStartPosition + Vector3.up * y;
+    }
+
+    // Jump physics math y = vi*t + g*t^2/2
+}
