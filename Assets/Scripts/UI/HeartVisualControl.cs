bca49e91c183b7531ea69b1b647854bb63d00889
vilius <viliusradz@gmail.com>
2024-07-19 23:27:50 +0300

    Add: Player health bar (h,j keybinds for testing) Remove: heart animations

--- Assets/Scripts/UI/HeartVisualControl.cs
+++ Assets/Scripts/UI/HeartVisualControl.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Animator))]
+
 public class HeartVisualControl : MonoBehaviour
 {
     public Image heartImage;
-    public Animator Animator { get; private set; }
     private Dictionary<Utils.HeartState, Sprite> sprites;
     private Utils.HeartState curState = Utils.HeartState.None;
 
@@ -16,35 +15,20 @@ public class HeartVisualControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void SetSprites(Dictionary<Utils.HeartState, Sprite> sprites) => this.sprites = sprites;
 
     public void ChangeHeart(Utils.HeartState state)
     {
-
-        // Via Math we get the index of the correct animation to display based on current state and new state
-
-        int animationIndex = (int)state;
-
-        animationIndex += (int)curState * 3;
-        if (isShadow)
-            animationIndex *= 2;
-
         curState = state;
-        Animator.SetInteger("heartState", animationIndex);
+        heartImage.sprite = sprites[curState];
+
+        if (curState == Utils.HeartState.None)
+            heartImage.enabled = false;
+        else
+            heartImage.enabled = true;
+
     }
 
-    public void SetSprite()
-    {
-        Animator.SetInteger("heartState", 0);
-        heartImage.sprite = sprites[curState];
-    }
 }
--- Assets/Scripts/UI/UIHealthControl.cs
+++ Assets/Scripts/UI/UIHealthControl.cs
@@ -11,7 +11,6 @@ public class UIHealthControl : MonoBehaviour
         public Sprite sprite;
     }
 
-
     public GameObject heartPrefab;
     public Transform healthBar;
 
@@ -36,19 +35,13 @@ public class UIHealthControl : MonoBehaviour
 
         player.OnHealthChanged += (sender, oldHealth, newHealth) =>
         {
-            
+            UpdateHearts(newHealth);
         };
 
 
         heartStates.ForEach(x => heartSprites.Add(x.state, x.sprite));
 
-        
-
-
-        var heart = Instantiate(heartPrefab, healthBar);
-        var hControl = heart.GetComponent<HeartVisualControl>();
-        hControl.SetSprites(heartSprites);
-        hearts.Add(hControl);
+        UpdateHearts(player.GetComponent<IHealth>().CurrentHealth);
     }
 
     // Update is called once per frame
@@ -56,8 +49,49 @@ public class UIHealthControl : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.H))
         {
-            state = !state;
-            hearts[0].ChangeHeart(Utils.HeartState.Full);
-        }   
+            FindAnyObjectByType<Player>().GetComponent<IHealth>().DealDamage(this, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            FindAnyObjectByType<Player>().GetComponent<IHealth>().DealDamage(this, 1);
+        }
+    }
+
+    private void UpdateHearts(float health)
+    {
+
+        var heartCount = (int)health;
+        var imageCount = (int)Mathf.Ceil(heartCount / 2.0f);
+
+        var addHeartAmount = imageCount - hearts.Count;
+        for (int i = 0; i < addHeartAmount; i++)
+            CreateNewHeart();
+
+        foreach (var heart in hearts)
+        {
+            if(heartCount > 1)
+            {
+                heart.ChangeHeart(Utils.HeartState.Full);
+                heartCount -= 2;
+            }
+            else if (heartCount == 1)
+            {
+                heart.ChangeHeart(Utils.HeartState.Half);
+                heartCount--;
+            }
+            else
+            {
+                heart.ChangeHeart(Utils.HeartState.None);
+            }
+
+        }
+    }
+
+    private void CreateNewHeart()
+    {
+        var heart = Instantiate(heartPrefab, healthBar);
+        var hControl = heart.GetComponent<HeartVisualControl>();
+        hControl.SetSprites(heartSprites);
+        hearts.Add(hControl);
     }
 }
