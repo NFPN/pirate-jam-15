90318b86e8056341dadd00acdb457c51db22ec4f
GG.mp4 <79061640+viliusradz@users.noreply.github.com>
2024-07-20 20:32:30 +0300

    add: text system in progress (#17)
    * Add: text system in progress small fixes
    
    * Fixed conflicts

---
+++ Assets/Scripts/TextSystem/TextHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "TextData", menuName = "ScriptableObjects/TextScriptableObject", order = 1)]
+public class TextHandler : ScriptableObject
+{
+    public List<TextObject> resources;
+}
+
+[System.Serializable]
+public class TextObject
+{
+    public string Name;
+    public List<TextLine> texts;
+}
+
+[System.Serializable]
+public class TextLine
+{
+    public int ID;
+    public string text;
+    public bool followSource;
+    public bool hasDuration = true;
+    public float textDuration = 5;
+    public Utils.TextBubbleEffects effect;
+}
