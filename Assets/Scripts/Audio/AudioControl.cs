79f211155fd4822677fc5b1b2509404164dc2c7f
vilius <viliusradz@gmail.com>
2024-07-27 17:14:13 +0300

    audio experimentation

--- Assets/Scripts/Audio/AudioControl.cs
+++ Assets/Scripts/Audio/AudioControl.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMOD.Studio;
+using FMODUnity;
 
 public class AudioControl : MonoBehaviour
 {
+    private List<EventInstance> eventInstances;
+
+    private EventInstance ambienEventInstance;
+
+    private void Start()
+    {
+        eventInstances = new();
+
+    }
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    private void InitAmbience(EventReference ambientEventRef)
+    {
+        
     }
 }
